e6e8f54ee2788b0fcb1d170777e547db23b88904
rishabhjain1712 <89248420+rishabhjain1712@users.noreply.github.com>
2023-01-30 22:52:48 +0530

    Add files via upload

---
+++ src/scripts/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9eec8ee9-f1b9-4e6d-83bb-e03c793bf74d';
+export const MODAL_CLOSE_SEC = 2.5;
